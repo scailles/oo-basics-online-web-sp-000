61f9c650a8df21508c7bacc60753dfac4c0c05dc
Shaina C <scailles23@gmail.com>
2020-12-31 03:33:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,25 @@ class Shoe
     @brand = brand
   end
   
+  def brand
+    @brand = brand
+  end
   
+  def color=(color)
+    @color=color
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(size)
+    @size=size
+  end
+  
+  def size
+    @size
+  end
   
   
 end
