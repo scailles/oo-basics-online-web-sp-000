ff86e0a7c9c6955d5443e99ec97d1aa7fa209a7f
Shaina C <scailles23@gmail.com>
2020-12-31 03:36:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -27,5 +27,25 @@ class Shoe
     @size
   end
   
+  def material=(material)
+    @material=material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition=condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble(condition)
+    @condition=new
+  end
+  
   
 end
